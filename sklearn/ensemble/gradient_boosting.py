898796a377b194bb115c496fc48f03a0f44ce78c
fabsig <fabio.sigrist@hslu.ch>
2018-03-08 10:53:39 +0100

    small change

--- sklearn/ensemble/gradient_boosting.py
+++ sklearn/ensemble/gradient_boosting.py
@@ -238,7 +238,7 @@ class LossFunction(six.with_metaclass(ABCMeta, object)):
 
     def update_terminal_regions(self, tree, X, y, residual, y_pred,
                                 sample_weight, sample_mask,
-                                learning_rate=1.0, k=0):
+                                learning_rate=1.0, k=0, NewtonWeights=True):
         """Update the terminal regions (=leaves) of the given tree and
         updates the current predictions of the model. Traverses tree
         and invokes template method `_update_terminal_region`.
@@ -269,15 +269,16 @@ class LossFunction(six.with_metaclass(ABCMeta, object)):
         # compute leaf for each sample in ``X``.
         terminal_regions = tree.apply(X)
 
-        # mask all which are not in sample mask.
-        masked_terminal_regions = terminal_regions.copy()
-        masked_terminal_regions[~sample_mask] = -1
+        if NewtonWeights:
+            # mask all which are not in sample mask.
+            masked_terminal_regions = terminal_regions.copy()
+            masked_terminal_regions[~sample_mask] = -1
 
-        # update each leaf (= perform line search)
-        for leaf in np.where(tree.children_left == TREE_LEAF)[0]:
-            self._update_terminal_region(tree, masked_terminal_regions,
-                                         leaf, X, y, residual,
-                                         y_pred[:, k], sample_weight)
+            # update each leaf (= perform line search)
+            for leaf in np.where(tree.children_left == TREE_LEAF)[0]:
+                self._update_terminal_region(tree, masked_terminal_regions,
+                                             leaf, X, y, residual,
+                                             y_pred[:, k], sample_weight)
 
         # update predictions (both in-bag and out-of-bag)
         y_pred[:, k] += (learning_rate
@@ -931,16 +932,17 @@ class BaseGradientBoosting(six.with_metaclass(ABCMeta, BaseEnsemble)):
                 tree.fit(X, residual, sample_weight=sample_weight,
                          check_input=False, X_idx_sorted=X_idx_sorted)
 
-            if NewtonWeights:
-                # update tree leaves ##CHANGE
-                if X_csr is not None:
-                    loss.update_terminal_regions(tree.tree_, X_csr, y, residual, y_pred,
-                                                 sample_weight, sample_mask,
-                                                 self.learning_rate, k=k)
-                else:
-                    loss.update_terminal_regions(tree.tree_, X, y, residual, y_pred,
-                                                 sample_weight, sample_mask,
-                                                 self.learning_rate, k=k)
+            # update tree leaves ##CHANGE
+            if X_csr is not None:
+                loss.update_terminal_regions(tree.tree_, X_csr, y, residual, y_pred,
+                                             sample_weight, sample_mask,
+                                             self.learning_rate, k=k,
+                                             NewtonWeights=NewtonWeights)
+            else:
+                loss.update_terminal_regions(tree.tree_, X, y, residual, y_pred,
+                                             sample_weight, sample_mask,
+                                             self.learning_rate, k=k,
+                                             NewtonWeights=NewtonWeights)
 
             # add tree to ensemble
             self.estimators_[i, k] = tree
